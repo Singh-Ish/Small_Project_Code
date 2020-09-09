c33ffa7d3931144a06a6b32326d0209843815ff5
Ishdeep Singh <Ishdeep.711@gmail.com>
2020-09-09 10:16:48 -0400

    adding the google scholar code

---
+++ PythonSmallProjects/GoogleScholar/author.py
@@ -0,0 +1,137 @@
+from scholarly import scholarly
+from fp.fp import FreeProxy
+import pandas as pd
+import json
+
+
+
+# for single author
+
+#search_query = scholarly.search_author('Andy Adler')
+#author = next(search_query).fill()
+
+#print(author)
+
+
+
+'''
+# multiple professors publication
+
+
+authors= ['Halim Yanikomeroglu','Andy Adler'] # name of professors to consider
+
+
+publication = pd.DataFrame(columns=['id','name', 'title','cites','year'])
+
+for a in authors:
+    search_query = scholarly.search_author(a)
+    author = next(search_query).fill()
+    # assigning id and name
+    id = author.id
+    name = author.name
+
+    # iterating all publications
+    for z in author.publications:
+
+        data = z.bib
+        #print(data)
+        pub_json = json.dumps(data)
+        pj = json.loads(pub_json)
+        if("year" in pj):
+            year= pj['year']
+        else:
+            year=0
+
+        title = pj['title']
+        cites = pj['cites']
+
+        publication = publication.append({'id':id, 'name':name,'title':title , 'cites':cites, 'year':year }, ignore_index=True)
+
+
+
+
+cdict = {'cites':int, 'year':int}
+
+#print(publication)
+#publication.astype({'cites':int,'year':int}).dtypes
+
+publication=publication.astype(cdict)
+
+publication.sort_values(by=['cites'],ascending=False,inplace=False)
+
+
+
+# export the dataframe to excel
+
+#publication.to_excel("publications.xlsx",engine='xlsxwriter')
+#print( "exporting the publications to excel ")
+
+
+print(publication.head(5))
+'''
+
+
+
+'''
+
+# multiple authors and their data
+
+# Retrieve the author's data, fill-in, and print
+authors= ['Halim Yanikomeroglu','Andy Adler'] # name of professors to consider 
+
+
+df = pd.DataFrame(columns=['id','name','affiliation','interest','citedby','citedbyyear','hindex','hindex5y','i10index','i10index5y'])
+for z in authors:
+    search_query = scholarly.search_author(z)
+    author = next(search_query).fill()
+    #print(author.name)
+
+    a = author.affiliation
+    b = author.citedby
+    c= author.cites_per_year
+    h = author.hindex
+    h5 = author.hindex5y
+    i = author.i10index
+    i10 = author.i10index5y
+    id = author.id
+    interest = author.interests
+    name = author.name
+
+    df =df.append({'id': id ,'name':name , 'affiliation':a, 'interest':interest ,'citedby':b ,'citedbyyear':c, 'hindex':h ,'hindex5y':h5, 'i10index':i, 'i10index5y':i10}, ignore_index=True)
+
+df.sort_values(by=['citedby'])
+'''
+
+
+'''
+#render dataframe as html
+html = df.to_html()
+
+#write html to file
+text_file = open("index.html", "w")
+text_file.write(html)
+text_file.close()
+'''
+#print(df)
+
+#df = df.to_excel('author.xlsx')
+#pub = pd.DataFrame()
+
+
+
+###### getting publications details
+
+def publicationDetails( p ):
+    # p is the passed publication name
+    search_query = scholarly.search_pubs(p)
+    pd = next(search_query)
+
+    print(pd.bibtex)
+    #dic.bib
+    #print(pd.bib.abstract)
+    #print(pd)
+
+
+
+pub='Uses and abuses of EIDORS: an extensible software base for EIT'
+publicationDetails(pub)
---
+++ PythonSmallProjects/GoogleScholar/main.py
@@ -0,0 +1,90 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_auth
+
+from scholarly import scholarly
+import pandas as pd
+import json
+
+
+##### dataframe from google scholar #####
+
+authors= ['Halim Yanikomeroglu','Andy Adler'] # name of professors to consider
+
+
+df = pd.DataFrame(columns=['id','name','affiliation','interest','citedby','citedbyyear','hindex','hindex5y','i10index','i10index5y'])
+for z in authors:
+    search_query = scholarly.search_author(z)
+    author = next(search_query).fill()
+    #print(author.name)
+
+    a = author.affiliation
+    b = author.citedby
+    c= author.cites_per_year
+    h = author.hindex
+    h5 = author.hindex5y
+    i = author.i10index
+    i10 = author.i10index5y
+    id = author.id
+    interest = author.interests
+    name = author.name
+
+    df =df.append({'id': id ,'name':name , 'affiliation':a, 'interest':interest ,'citedby':b ,'citedbyyear':c, 'hindex':h ,'hindex5y':h5, 'i10index':i, 'i10index5y':i10}, ignore_index=True)
+
+df.sort_values(by=['citedby'])
+
+
+
+
+
+
+
+##### datframe end ######
+
+
+
+
+
+##### html rendering
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['hello', 'world']
+]
+
+app = dash.Dash('auth')
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+
+
+app.layout = html.Div([
+
+
+    html.H3(
+        children='Publications/Patents Having 100 or More Citations on Google Scholar',
+        style={
+            'textAlign': 'left',
+            'textSize': '12px',
+            'textColor':'red'
+                  }
+        ),
+
+    dcc.Input(id='my-id', value='Dash App', type='text'),
+    html.Div(id='my-div')
+])
+
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
+
+
+
+if __name__ == '__main__':
+    app.run_server()
