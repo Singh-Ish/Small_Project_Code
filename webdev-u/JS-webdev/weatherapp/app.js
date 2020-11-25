0e87cc9098bb18f4caaa8cd4a43903100a1aac23
Ishdeep Singh <Ishdeep.711@gmail.com>
2020-11-24 20:20:48 -0500

    like weather app with live data and entering city name

--- webdev-u/JS-webdev/weatherapp/app.js
+++ webdev-u/JS-webdev/weatherapp/app.js
@@ -1,12 +1,56 @@
-const express = require("express")
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser")
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
 
-res.send("server is up and running fine ")
+    res.sendFile(__dirname +"/index.html");
 
 })
+
+app.post("/",function(req,res){
+    
+    console.log(req.body.cityName)
+    //console.log("Post request recieved");
+
+    const city = req.body.cityName
+    const apikey = "11afbb6bf90a9260003265292a3a86ec#"
+    const unit = "metric"
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=" + unit + "&appid=" + apikey
+
+    https.get(url, function(response){
+
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data)
+            //console.log(weatherData);
+
+            //getting the weather data 
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const iconImageUrl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
+            
+            res.write("<p> the weather is currently "+ weatherDescription + "</p>")
+            res.write("<h1>The temperature in " + city+ " is " + temp +" degree celcius. </h1>")
+            res.write("<img src=" + iconImageUrl + ">")
+            res.send()
+
+        })
+    })
+
+
+})
+
+
+
+//     res.send("server is up and running fine ")
 
 
 
